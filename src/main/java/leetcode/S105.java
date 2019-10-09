a628f1f81864f94478f88b2cd801897e6b05ce34
origin <15761635397@163.com>
2019-10-09 20:08:22 +0800

    add 105

--- src/main/java/leetcode/S105.java
+++ src/main/java/leetcode/S105.java
@@ -1,8 +1,31 @@
 package leetcode;
 
+import datastructs.TreeNode;
+
+/**
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ *
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ */
 public class S105 {
 
     public static void main(String[] args) {
 
+    }
+
+    class Solution {
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+            if (preorder == null || preorder.length == 0){
+                return null;
+            }
+            int midPosition = 0;
+            TreeNode root = new TreeNode(preorder[0]);
+            while (midPosition < preorder.length){
+
+
+            }
+            return root;
+        }
     }
 }
