c7e7882c8215b5b626dc8f11ad12200180de92d6
origin <15761635397@163.com>
2019-10-09 16:16:43 +0800

    init treeNode

---
+++ src/main/java/datastructs/TreeNode.java
@@ -0,0 +1,10 @@
+package datastructs;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x){
+        this.val = x;
+    }
+}
---
+++ src/main/java/datastructs/package-info.java
@@ -0,0 +1,4 @@
+package datastructs;
+/**
+ * 与 Leetcode 一致的数据结构
+ */
